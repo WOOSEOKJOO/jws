e85e907fb317f419f17321345fe4e2d1b93a381b
WOOSEOKJOO <87465067+WOOSEOKJOO@users.noreply.github.com>
2021-07-16 15:28:29 +0900

    2021-07-16 15:30

---
+++ java/First/src/lesson00/E002_ArithmeticOperator.java
@@ -0,0 +1,19 @@
+package lesson00;
+
+public class E002_ArithmeticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a = 10;
+       int b = 20;
+       
+//       int c;
+//       c = a + b;
+       
+       System.out.println(a+ "+"+ b + "="+ a+b);
+       
+       
+       System.out.println()
+	}
+
+}
---
+++ java/First/src/lesson00/E003_AssignmentOperators.java
@@ -0,0 +1,26 @@
+package lesson00;
+
+public class E003_AssignmentOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// create variables
+	    int a = 4;
+	    int var;
+
+	    // assign value using =
+	    var = a;
+	    System.out.println("var using =: " + var);
+
+	    // assign value using =+
+	    var += a;
+	    System.out.println("var using +=: " + var);
+
+	    // assign value using =*
+	    var *= a;
+	    System.out.println("var using *=: " + var);      
+		
+		
+	}//end of main()
+
+}//end of class
